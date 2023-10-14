fbf9f560212e288f2e7e6f954cb0d5194b72e411
NicoDestephen <ndestephen@gmail.com>
2023-10-14 23:23:26 +0200

    create repo for composer quest 1

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__ . '/../src/Hello.php';
+
+use App\Hello;
+
+$message = new Hello();
---
+++ src/Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class Hello
+{
+    private string $message;
+
+    public function talk($message): string
+    {
+        $message = "Hello World !";
+        return $message;  
+    }
+}
